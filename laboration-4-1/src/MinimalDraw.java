d2b4a81803ef9d555f2cf834e332874c84d5381e
Joelpettersson35 <joelpett101@student.kau.se>
2025-12-14 13:00:09 +0100

    labb 4-1 klar

--- laboration-4-1/src/MinimalDraw.java
+++ laboration-4-1/src/MinimalDraw.java
@@ -8,7 +8,7 @@ import view.*;
 
 public class MinimalDraw {
 	public static void main(String[] args) {
-		JFrame j=new JFrame();
+		/* JFrame j=new JFrame();
 		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		DrawingContainer dc=new DrawingContainer(); 
 		DrawingUtil da=new DrawingUtil();	
@@ -29,6 +29,25 @@ public class MinimalDraw {
 		dc.add(r2);
 		dc.remove(r);
 		j.repaint();
+		*/
+		
+		JFrame j=new JFrame();
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		
+		DrawingFacade df = new DrawingFacade(); //referens till fasaden för model-skiktet
+		df.addRect(320,200,80,80,5,Color.GREEN,Color.PINK);
+		df.addCircle(150,150,50,250,20,Color.BLUE, Color.CYAN);
+		df.saveDrawing(); //sparar figurer
+		df.addLine(3,3,50,50,1,Color.RED); //denna rad tydliggör att det är data från filen som laddas, eftersom denna linje inte kommer med
+		
+		df.loadDrawing(); //laddar figurerna från fil
+		DrawingPanel dp = new DrawingPanel(df.getContainer()); 
+		dp.setBackground(Color.WHITE); 
+		
+		j.add(dp);
+		j.setSize(500, 500); 
+		j.setVisible(true);
+		
 	}
 }
 
